8d81f129b2f1578c2db8844a9f950396c7fd2fc3
Trinity Rolling <1168285+trinityr@users.noreply.github.com>
2019-01-16 08:44:51 -0600

    Antifrieza - changes as of 1/16/19
    Added Wifi connection, and data logging to an influxdb

--- antifrieza/antifrieza.ino
+++ antifrieza/antifrieza.ino
@@ -1,20 +1,3 @@
-#include <OLEDDisplay.h>
-#include <OLEDDisplayFonts.h>
-#include <OLEDDisplayUi.h>
-#include <SH1106.h>
-#include <SH1106Brzo.h>
-#include <SH1106Spi.h>
-#include <SH1106Wire.h>
-#include <SSD1306.h>
-#include <SSD1306Brzo.h>
-#include <SSD1306Spi.h>
-#include <SSD1306Wire.h>
-
-#include <DS18B20Events.h>
-
-#include <Config.h>
-#include <EasyBuzzer.h>
-
 #include <BearSSLHelpers.h>
 #include <CertStoreBearSSL.h>
 #include <ESP8266WiFi.h>
@@ -33,54 +16,129 @@
 #include <WiFiServerSecureAxTLS.h>
 #include <WiFiServerSecureBearSSL.h>
 #include <WiFiUdp.h>
-  
-void setup() {
-  
-  Serial.begin(115200);
+
+#include <DS18B20Events.h>
+#include <DallasTemperature.h>
+#include <Config.h>
+
+
+
+// Replace with your network details
+const char* ssid = "Beyond9-5 - WiFi";     //  your network SSID (name) 
+const char* pass = "Tucker43";    // your network password
+
+// the IP address of your InfluxDB host
+//35.188.140.132
+byte host[] = {35, 188, 140, 132};
+
+// the port that the InfluxDB UDP plugin is listening on
+int port = 8089;
+
+
+#define PiezoPin 4 //D2 on NodeMCU
+#define ONE_WIRE_BUS 5 //D1 on NodeMCU
+// How many bits to use for temperature values: 9, 10, 11 or 12
+#define SENSOR_RESOLUTION 9
+// Index of sensors connected to data pin, default: 0
+#define SENSOR_INDEX 0
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+DeviceAddress sensorDeviceAddress;
+
+WiFiServer server(80);
+
+WiFiUDP udp;
+
+
+
+void setup () {
+
+  Serial.begin(9600);
+
+   // Connecting to WiFi network
   Serial.println();
-
-  WiFi.begin("SSID", "password");
-
-  Serial.print("Connecting");
-  while (WiFi.status() != WL_CONNECTED)
-  {
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, pass);
+  
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  Serial.println();
-
-  Serial.print("Connected, IP address: ");
+  Serial.println("");
+  Serial.println("WiFi connected");
+  
+  // Starting the web server
+  server.begin();
+  Serial.println("Web server running. Waiting for the ESP IP...");
+  delay(10000);
+  
+  // Printing the ESP IP address
   Serial.println(WiFi.localIP());
 
+  sensors.begin();
+  sensors.getAddress(sensorDeviceAddress, 0);
+  sensors.setResolution(sensorDeviceAddress, SENSOR_RESOLUTION);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  const port = 80;
-  const char * host = "10.3.1.10"; //Static IP 
+   // Call sensors.requestTemperatures() to issue a global temperature and Requests to all devices on the bus
+  sensors.requestTemperatures(); 
+  // Serial.print("Celsius temperature: ");
+  // Why "byIndex"? You can have more than one IC on the same bus. 0 refers to the first IC on the wire
+  // Serial.print(sensors.getTempFByIndex(0)); 
+  float TempF = sensors.getTempFByIndex(SENSOR_INDEX);
 
-  Serial.print("connecting to ");
-  Serial.printIn(host);
+  String line, temperature;
+  temperature = String(TempF,4);
 
-  // Use WiFiClient class to create TCP connections
-  WiFiClient client;
-
-  if ('client.connect(host, port)) (
-    Serial.printIn("connect failed");
-    Serial.printIn("wait 5 sec...");
-    delay(5000);
-    return;
-    )
-    Serial.printIn("Connected to the host!");
-
-    //read back one from server
-    String line = client.readStringUntil('\r');
-    Serial.printIn(line);
-
-    Serial.printIn("closing connection");
-    client.stop();
-
-    Serial.printIn("wait 5 sec...");
-    delay(5000);
-    
+  if (TempF > 75.0) {
+    //Buzzer
+    tone(PiezoPin, 1000, 1000);
+    //Log
+    line = String("temperature value=" + temperature);
+    Serial.println(line);
+    Serial.print("Over Temp! ");
+    Serial.print(TempF, 4);
+    Serial.println(" Fahrenheit");
+      // send the packet
+       Serial.println("Sending UDP packet...");
+       udp.beginPacket(host, port);
+       udp.print(line);
+       udp.endPacket();
+    //Email
+  }
+  else if (TempF < 50.0) {
+    //Buzzer
+    tone(PiezoPin,500,1000);
+   //Log
+    line = String("temperature value=" + temperature);
+    Serial.println(line);
+    Serial.print("Under Temp! ");
+    Serial.print(TempF, 4);
+    Serial.println(" Fahrenheit");
+      // send the packet
+       Serial.println("Sending UDP packet...");
+       udp.beginPacket(host, port);
+       udp.print(line);
+       udp.endPacket();
+    //Email
+  }
+  else {
+    //Log and move on
+    //Log
+    line = String("temperature value=" + temperature);
+    Serial.println(line);
+    Serial.print(TempF, 4);
+    Serial.println(" Fahrenheit");
+      // send the packet
+       Serial.println("Sending UDP packet...");
+       udp.beginPacket(host, port);
+       udp.print(line);
+       udp.endPacket();
+  }
+  
+  delay(300000);
 }
